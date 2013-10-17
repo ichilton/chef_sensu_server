d1f1667829b23da92831c1a0869b3de7c2757e85
Ian Chilton <ian@ichilton.co.uk>
2013-10-17 15:29:37 +0000

    Initial commit of the default recipe & attributes

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,12 @@
 # Cookbook Name:: sensu_server
 # Recipe:: default
 #
-# Copyright (C) 2013 YOUR_NAME
-# 
-# All rights reserved - Do Not Redistribute
-#
+
+include_recipe "sensu::rabbitmq"
+include_recipe "sensu::redis"
+
+include_recipe 'sensu::default'
+
+include_recipe "sensu::server_service"
+include_recipe "sensu::api_service"
+include_recipe "sensu::dashboard_service"
