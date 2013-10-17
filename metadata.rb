0baa9692878d0fa96630bf0c6caef15bbc1599ab
Ian Chilton <ian@ichilton.co.uk>
2013-10-17 15:25:38 +0000

    Add content to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,15 @@
 name             'sensu_server'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Ian Chilton'
+maintainer_email 'ian@ichilton.co.uk'
 license          'All rights reserved'
-description      'Installs/Configures sensu_server'
+description      'Setup a Sensu Server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+supports 'ubuntu'
+supports 'debian'
+supports 'centos'
+supports 'redhat'
+supports 'fedora'
+
+depends 'sensu'
